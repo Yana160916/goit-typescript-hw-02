c7c4b81b82e3ba6788ff38df1216756d7e30642d
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-05-17 18:58:30 +0300

    update

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar/SearchBar.tsx';
 import ImageGallery from '../components/ImageGallery/ImageGallery.tsx';
 import Loader from '../components/Loader/Loader.tsx';
